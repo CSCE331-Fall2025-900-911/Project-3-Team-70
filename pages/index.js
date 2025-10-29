421ef828fb0fc2c099d827512a2ca814a27ab299
Leo <leo.cuzela@gmail.com>
2025-10-29 14:35:15 -0500

    Added shell project so vercel can deploy

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first Next.js app deployed on Vercel 🚀</p>
+    </div>
+  );
+}
