4f937bfe99246dbae980c0a9225314c675d8ce50
JaredSpotts <jaredaspotts@gmail.com>
2025-12-10 13:39:41 -0600

    Bullshit commit while i try to fix kitchen

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -43,7 +43,13 @@ async function handleCreateOrder(req, res) {
     }
 
     // Compute order total
-    const orderTotal = Number(req.body.finalTotal ?? 0);
+    const orderTotal = items.reduce(
+      (sum, item) =>
+        sum +
+        Number(item.priceAtPurchase || 0) *
+          Number(item.quantity || 0),
+      0
+    );
 
     const nextOrderResult = await query(
       "SELECT COALESCE(MAX(orderID), 0) + 1 AS nextId FROM ordertest"
